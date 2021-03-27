9c2fbdc85b5ad5b67cc23baaab34a30e8ca50c11
Sebastian Brear <ssbrear@ucdavis.edu>
2021-03-26 22:51:40 -0700

    Math is working properly. Values in each stock vary from 0.1 to 1.9 every second, reflected in the size of the bar on the chart. The styling is broken though, because the bars do not stay aligned at the bottom.

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Dashboard() {
+  let stocks = [1, 1, 1, 1, 1];
+  useEffect(() => {
+    setInterval(loop, 1000);
+  });
+  const loop = () => {
+    let changes = [];
+    for (let i = 0; i < 5; i++) {
+      changes.push(Math.random() - Math.random());
+    }
+    stocks = stocks.map((x, i) => {
+      let newX = x + changes[i];
+      if (newX < 0.1) return 0.1;
+      if (newX > 1.9) return 1.9;
+      return newX;
+    });
+    changeBars();
+  };
+  const changeBars = () => {
+    const barList = document.getElementById("stock-bars").children;
+    for (let i = 0; i < 5; i++) {
+      const value = stocks[i];
+      barList[i].style.height = String(355.56 * value + 4.444) + "px";
+    }
+  };
   return (
     <main id="dashboard-main">
+      <section id="time-control">
+        <button id="play-button">Play</button>
+        <button id="pause-button">Pause</button>
+        <button id="stop-button">Stop</button>
+      </section>
       <section id="player-info">
         <p>Wallet</p>
         <p>
