b8bcca4e2e563671a624daabca95a6eed100ff51
Sebastian Brear <ssbrear@ucdavis.edu>
2021-03-26 20:34:56 -0700

    Created homepage with minimalistic styling and no functional elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div id="title">Stonks</div>
       </header>
+      <main>
+        <p>
+          Test yourself in a stonk market that is interpreted as being
+          completely random!
+        </p>
+        <p>Are you able to come out on top?</p>
+        <img alt="Game dashboard" src="https://via.placeholder.com/450"></img>
+        <p>There will be 5 total stocks that change share value every second.</p>
+        <p>You will be given $100 to begin, with each stock starting at $1 per share.</p>
+        <button>Begin</button>
+      </main>
     </div>
   );
 }
